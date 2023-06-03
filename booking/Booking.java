dff8678abc47dddeebbc97ceee13db25635722b4
Allwan Setyo R <117294030+allwzz@users.noreply.github.com>
2023-06-03 19:12:27 +0700

    Add files via upload

---
+++ booking/Booking.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package booking;
+
+/**
+ *
+ * @author allwanzx
+ */
+public class Booking {
+    
+ 
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginPage bf = new LoginPage ();
+        
+        bf.setVisible(true);
+        // TODO code application logic here
+    }
+
+    
+}
---
+++ booking/Payment.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package booking;
+
+/**
+ *
+ * @author allwanzx
+ */
+public class Payment {
+    //modul 6 setter getter
+    private double totalPayment;
+
+    public void setTotalPayment(double totalPayment) {
+        this.totalPayment = totalPayment;
+    }
+
+    public double getTotalPayment() {
+        return totalPayment;
+    }
+}
+
